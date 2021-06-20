a3ada2dd288bbea5cfb42586a5aec867a3bb7470
Anthony Balsamo <anthonybalsamo@Anthonys-MacBook-Pro.local>
2021-06-20 14:44:44 -0500

    5.Styled Components AboutSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,66 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StlyedHide>
             <h2>Create</h2>
-          </div>
-          <div className="hide">
+          </StlyedHide>
+          <StlyedHide>
             <h2>
               <span>Code</span>
             </h2>
-          </div>
-          <div className="hide">
+          </StlyedHide>
+          <StlyedHide>
             <h2>Animate</h2>
-          </div>
+          </StlyedHide>
           <p>Contact me for your ideas and let's give them life. </p>
           <button>Ping Me</button>
         </div>
-        <div className="image">
-          <img src={home1} alt="profile of Anthony" />
-        </div>
-      </div>
-    </div>
+      </StyledDescription>
+      <StyledImage>
+        <img src={home1} alt="profile of Anthony" />
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+//Styled Component
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 80%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const StlyedHide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
